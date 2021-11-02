aa6fa956dea9bf81355c5c69542c48372c11c0f2
tuily <brizolla.tuily@gmail.com>
2021-11-02 14:52:42 -0300

    Add sass support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+// next.config.js
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-};
+  cssModules: true,
+});
