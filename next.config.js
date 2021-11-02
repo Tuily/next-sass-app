5b5c9b989d3215df6c073cfbf6300fcb56c1eec0
tuily <brizolla.tuily@gmail.com>
2021-11-02 15:05:12 -0300

    Add sass support config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 // next.config.js
+
+const path = require('path');
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true,
+  /* bydefault config  option Read For More Optios
+here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
   cssModules: true,
 });
+module.exports = {
+  /* Add Your Scss File Folder Path Here */
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Header from '../components/Header';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
     </div>
   );
