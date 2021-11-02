075447a7e26fb98e7f3bcc1eef15a82a9ae5a35b
tuily <brizolla.tuily@gmail.com>
2021-11-02 14:49:35 -0300

    Update header msg

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 import styles from './Header.module.scss';
 
-const Header = () => <h1 className={styles.header}>I'm a header!</h1>;
+const Header = () => <h1 className={styles.header}>Im a header!</h1>;
 export default Header;
